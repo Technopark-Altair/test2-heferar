b553e4914510d4e1103d39f46980e0848924896f
heferar <cen9.orl@mail.ru>
2020-11-11 21:44:16 +0300

    kr2
    kr2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,54 @@
+import { BinaryOperator } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { Stream } from 'stream';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+ export class AppComponent {
   title = 'my-app';
-firstOperand: number;
-secondOperand: number;
-operator:string;
-result:number
-errorMeesage:string
-
+operator2:string;
+operator: string;
+    firstOperand: number;
+    secondOperand: number;
+     errorMessage: string;
+     result: number;
+ 
+  
 doCalc(){
-if (this.firstOperand != null && this.secondOperand !=null && this.operator != null){
-}else if  (this.operator == "||") {
-     if (Boolean (1) && Boolean(1)){}
-     this.result =1;
-     if (Boolean (0) && Boolean(1)){}
-     this.result =null;
-     this.errorMeesage = "Если в выражении есть хотя бы  один Нуль,выражение равно Нулю"
-     if (Boolean (1) && Boolean(0)){}
-     this.result =null;
-     this.errorMeesage = "Если в выражении есть хотя бы  один Нуль,выражение равно Нулю"
-     if (Boolean (0) && Boolean(0)){}
-     this.result =null;
-     this.errorMeesage = "Если в выражении есть хотя бы  один Нуль,выражение равно Нулю"
-}else if (this.operator ="||") {
-       if (Boolean (1) || Boolean(1)){
-        this.result = 1;
-       if (Boolean (1) || Boolean(0)){
-        this.result = 1;
-       if (Boolean (0) || Boolean(1)){
-        this.result = 1;
-       if (Boolean (0) || Boolean(0)){}
-        this.result = 0;
-        this.errorMeesage="Если в выражении нет хотя бы одной единицы ,выражение равно Нулю"
-       }else if  (this.operator == "!") {
-         if (this.result =0)
+  if (((this. firstOperand == 1) || (this. firstOperand == 0)) && ((this.secondOperand == 1) || (this.secondOperand == 0))) {
+    if (this. firstOperand != null && this.operator != null && this.secondOperand != null) {
+      if (this.operator == '&&') {
+        if (this. firstOperand == 1 && this.secondOperand == 1) {
           this.result = 1
-          if (this.result =1)
+          this.errorMessage = "Нет ошибки"
+        } else {
           this.result = 0
+          this.errorMessage = "Нет ошибки"
         }
-
-     }
-   }
- }
+      } else if (this.operator == '||') {
+        if ((this. firstOperand + this.secondOperand == 1)){
+          this.result = 0
+          this.errorMessage = "Произведение равно 1 когда все множители равны 1"
+         if (this. firstOperand + this.secondOperand == 2){
+          this.result = 1
+          this.errorMessage = "Нет ошибки"
+        }
+      }
+    }
+  } else {
+    this.result = null
+    this.errorMessage = "Нет ошибки"
+  } if (this.operator2 == '!') {
+      if (this.result == 1) {
+        this.result = 0
+      } else {
+        if (this.result == 1) 
+        this.result = 1
+      }
   }
 }
-
-
+}
+ }
